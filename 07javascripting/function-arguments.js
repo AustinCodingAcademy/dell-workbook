0b53e4017d6f38d51a464a7a83dc42251b8a2b6e
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-11-10 22:52:53 -0600

    changed single tab to two spaces

--- 07javascripting/function-arguments.js
+++ 07javascripting/function-arguments.js
@@ -1,5 +1,5 @@
 function math(addTotal, multiplyOne, multiplyTwo){
-	return addTotal + multiplyOne * multiplyTwo;
+  return addTotal + multiplyOne * multiplyTwo;
 }
 
 console.log(math(53, 61, 67));
