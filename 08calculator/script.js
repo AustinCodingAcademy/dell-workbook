ee59c130ca03614a59b7b069573331224a30aa99
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-11-14 18:52:54 -0600

    fixed the missing DOCTYPE

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,24 +1,26 @@
 'use strict';
 
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
-  function addNumber(num) {
-    document.querySelector("#results").value += num;
-  }
-
-  function clearResults() {
-    document.querySelector("#results").value = "";
-  }
-
-  function addition() {
-    document.querySelector("#results").value += "+";
-  }
-
-  function equals() {
-    document.querySelector("#results").value = eval(document.querySelector("#results").value);
-  }
-
-  function deleteLast() {
-    let current = document.querySelector("#results").value;
-    document.querySelector("#results").value = current.slice(0, -1);
-  }
+ 
 });
