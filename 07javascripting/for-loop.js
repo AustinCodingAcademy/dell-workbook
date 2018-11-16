bd9fb495b4c6180088fa13c0f9eb865ecdf9546b
Golla <Mounika_Golla@Dell.com>
2018-11-16 14:57:21 -0600

    Javascripting

---
+++ 07javascripting/for-loop.js
@@ -0,0 +1,22 @@
+// var start = 1;
+// for(var i=start;i<=100;i++)
+// {
+//     if(i%3 ==0 && i%5 ==0)
+//      console.log("FizzBuzz");
+//      else if(i%3 ==0)
+//      console.log("Fizz");
+//      else if(i%5 ==0)
+//      console.log("Buzz");
+//      else
+//      console.log(i)
+      
+     
+// }
+
+var total = 0;
+var limit = 10;
+for(var i =0 ;i<limit;i++)
+{
+  total+=i;
+}
+console.log(total);
