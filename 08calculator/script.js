f927546f8d74e43c1cb0998d65a43f7e1d9b9704
Lam, Alice <Alice_Lam@Dell.com>
2018-11-16 16:42:29 -0600

    adding in other function buttons

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -9,6 +9,15 @@ function clearResults() {
 function addition() {
   document.querySelector("#results").value += "+";
 }
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+function multiplication() {
+  document.querySelector("#results").value += "*";
+}
+function division() {
+  document.querySelector("#results").value += "/";
+}
 
 function equals() {
   document.querySelector("#results").value = eval(
