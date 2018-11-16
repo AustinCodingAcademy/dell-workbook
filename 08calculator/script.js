496993d6c943eab7938c4f4ded78052c0db1728e
Yenugu <Indumathi_Yenugu@Dell.com>
2018-11-16 16:19:24 -0600

    Fixed Errors in calc

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,41 +1,37 @@
-"use strict";
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  function addNumber(num) {
-    document.querySelector("#results").value += num;
-  }
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
 
-  function clearResults() {
-    document.querySelector("#results").value = "";
-  }
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+function sub() {
+  document.querySelector("#results").value += "-";
+}
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
 
-  function addition() {
-    document.querySelector("#results").value += "+";
-  }
-  function sub() {
-    document.querySelector("#results").value += "-";
-  }
-  function multiply() {
-    document.querySelector("#results").value += "*";
-  }
+function divide() {
+  document.querySelector("#results").value += "/";
+}
 
-  function divide() {
-    document.querySelector("#results").value += "/";
-  }
+function flip() {
+  document.querySelector("#results").value =
+    -1 * document.querySelector("#results").value;
+}
 
-  function flip() {
-    document.querySelector("#results").value =
-      -1 * document.querySelector("#results").value;
-  }
+function equals() {
+  document.querySelector("#results").value = eval(
+    document.querySelector("#results").value
+  );
+}
 
-  function equals() {
-    document.querySelector("#results").value = eval(
-      document.querySelector("#results").value
-    );
-  }
-
-  function deleteLast() {
-    let current = document.querySelector("#results").value;
-    document.querySelector("#results").value = current.slice(0, -1);
-  }
-});
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
