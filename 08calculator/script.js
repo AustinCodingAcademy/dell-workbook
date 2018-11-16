1bddb064aa4f21dfc3ac61243cc603ff10b1b8cf
Patti Parnell <Patricia_Parnell@dell.com>
2018-11-16 16:12:21 -0600

    cal1

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,5 +1,44 @@
-'use strict';
+"use strict";
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function equals() {
+  if (document.querySelector("#results").value !== "") {
+    document.querySelector("#results").value = eval(
+      document.querySelector("#results").value
+    );
+  }
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiple() {
+  document.querySelector("#results").value += "*";
+}
+
+function divide() {
+  document.querySelector("#results").value += "/";
+}
+
+function changeSign() {
+  if (!isNaN(Number(document.querySelector("#results").value))) {
+    document.querySelector("#results").value =
+      eval(document.querySelector("#results").value) * -1;
+  }
+}
