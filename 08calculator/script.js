84135e40636a551bb13f8f51c028dcc88a7a00af
Burns <Catherine_Burns@Dell.com>
2018-11-19 14:03:25 -0600

    calculator updates

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,5 +1,45 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+// });
+
+
+function addNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+function divide() {
+  document.querySelector("#results").value += "/";
+}
+function plusminus() {
+  var mynum = document.querySelector("#results").value 
+  var newnum = mynum*(-1); 
+  console.log(newnum)
+  document.querySelector("#results").value = newnum;
+  // let before = eval(document.querySelector("#results").value);
+  // let after = before*(-1);
+  // document.querySelector("#results").value += after;
+  //document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
