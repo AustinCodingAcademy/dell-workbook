a81cf9044dbb7947f5e4f4c814584ee725aa4d4c
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-11-21 06:56:27 -0600

    add if only child check to see if the cell can add more object

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -2,21 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", function (event) {
   var color = 'black';
-  
+
   document.querySelectorAll('[data-cell]').forEach(function (cell) {
     cell.addEventListener('click', addObject);
   })
 
   document.querySelector('#clear').addEventListener("click", clear);
-  
-  function clear () {
+
+  function clear() {
     document.querySelectorAll('[data-cell]').forEach(function (cell) {
       cell.innerHTML = "";
     })
   }
 
-  function addObject () {
-    this.insertAdjacentHTML('afterbegin', `<div class="disc ${color}"></div>`);
-    color = (color === 'black') ? 'red' : 'black';
+  function addObject() {
+    if (!this.firstChild) {
+      this.insertAdjacentHTML('afterbegin', `<div class="disc ${color}"></div>`);
+      color = (color === 'black') ? 'red' : 'black';
+    }
   }
+
+  
 });
