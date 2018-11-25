fec5734cc68e78f871bd1176f5664493933b3e9b
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-11-25 09:40:20 -0600

    refactor methods to make more readable

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -4,23 +4,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var style = 'o';
   var selectedOCells = [];
   var selectedXCells = [];
-  var winnerPositions = [];
-  winnerPositions = [
-    { "pos1": "0", "pos2": "1", "pos3": "2" }
-    , { "pos1": "3", "pos2": "4", "pos3": "5" }
-    , { "pos1": "6", "pos2": "7", "pos3": "8" }
-    , { "pos1": "0", "pos2": "3", "pos3": "6" }
-    , { "pos1": "1", "pos2": "4", "pos3": "7" }
-    , { "pos1": "2", "pos2": "5", "pos3": "8" }
-    , { "pos1": "0", "pos2": "4", "pos3": "8" }
-    , { "pos1": "2", "pos2": "4", "pos3": "6" }
-  ];
 
   document.querySelectorAll('[data-cell]').forEach(function (cell) {
     cell.addEventListener('click', addObject);
   })
 
   document.querySelector('#clear').addEventListener("click", clear);
+
 
   function clear() {
     document.querySelectorAll('[data-cell]').forEach(function (cell) {
@@ -30,34 +20,45 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelector('#announce-winner').innerHTML = "";
   }
 
+  function getWinnerPositions() {
+    return [
+      { "pos1": "0", "pos2": "1", "pos3": "2" }
+      , { "pos1": "3", "pos2": "4", "pos3": "5" }
+      , { "pos1": "6", "pos2": "7", "pos3": "8" }
+      , { "pos1": "0", "pos2": "3", "pos3": "6" }
+      , { "pos1": "1", "pos2": "4", "pos3": "7" }
+      , { "pos1": "2", "pos2": "5", "pos3": "8" }
+      , { "pos1": "0", "pos2": "4", "pos3": "8" }
+      , { "pos1": "2", "pos2": "4", "pos3": "6" }
+    ];
+  }
+
   function addObject() {
     if (!this.firstChild) {
       this.insertAdjacentHTML('afterbegin', `<div class="disc ${style}">${style}</div>`);
-      var checkedCells = 0;
-
-      if (style === 'o') {
-        selectedOCells.push(this.getAttribute("data-cell"));
-        checkedCells = selectedOCells.length;
-      } else if (style === 'x') {
-        selectedXCells.push(this.getAttribute("data-cell"));
-        checkedCells = selectedXCells.length;
-      }
-
-      if (checkedCells > 2)
+      var checkedCells = addSelectedCellToArray(this).length;
+      if (checkedCells > 2) {
         checkWinner(this);
+      }
 
       style = (style === 'o') ? 'x' : 'o';
     }
   }
 
+  function addSelectedCellToArray(clickedCell) {
+    if (style === 'o') {
+      selectedOCells.push(clickedCell.getAttribute("data-cell"));
+      return selectedOCells;
+    } else if (style === 'x') {
+      selectedXCells.push(clickedCell.getAttribute("data-cell"));
+      return selectedXCells;
+    }
+    return [];
+  }
+
   function checkWinner(clickedCell) {
-    var clickedValue = clickedCell.getAttribute("data-cell");
+    var filteredWinnerPos = getAllWinnerPos(clickedCell);
 
-    var filteredWinnerPos = winnerPositions.filter(function (winnerPos) {
-      return clickedValue === winnerPos.pos1 || clickedValue === winnerPos.pos2 || clickedValue === winnerPos.pos3;
-    });
-
-    var foundWinner = false;
     var selectedValues = selectedOCells;
     if (style === 'x') {
       selectedValues = selectedXCells;
@@ -65,14 +66,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     for (let i = 0; i < filteredWinnerPos.length; i++) {
       if (selectedValues.includes(filteredWinnerPos[i].pos1) & selectedValues.includes(filteredWinnerPos[i].pos2) & selectedValues.includes(filteredWinnerPos[i].pos3)) {
-        foundWinner = true;
+        endGame();
         break;
       }
     }
-    
-    if (foundWinner === true) {
-      endGame();
-    }
+  }
+
+  function getAllWinnerPos(clickedCell) {
+    var clickedValue = clickedCell.getAttribute("data-cell");
+    var filteredWinnerPos = getWinnerPositions().filter(function (winnerPos) {
+      return clickedValue === winnerPos.pos1 || clickedValue === winnerPos.pos2 || clickedValue === winnerPos.pos3;
+    });
+    return filteredWinnerPos;
   }
 
   function endGame() {
