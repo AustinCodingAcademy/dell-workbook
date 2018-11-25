3f1308065c26d1089588fbdf2926b9369ac0cb85
Golla <Mounika_Golla@Dell.com>
2018-11-24 19:58:08 -0600

    assignments

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,5 +1,31 @@
-'use strict';
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+function addNumber(num) {
+  document.querySelector("#results").value = num;
+}
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+function Subtraction() {
+  document.querySelector("#results").value += "-";
+}
+function Multiplication() {
+  document.querySelector("#results").value += "*";
+}
+function Division() {
+  document.querySelector("#results").value += "/";
+}
+function Negate() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = -Math.abs(current.slice(0, -1));
+}
+function equals() {
+  document.querySelector("#results").value = eval(document.querySelector("#results").value);
+}
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
-});
+
