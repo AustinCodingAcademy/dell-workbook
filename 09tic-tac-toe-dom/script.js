77296df1fcaf291284248a86464c4cbce5855aeb
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-11-25 09:26:10 -0600

    Added checkWinner function

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var style = 'o';
   var selectedOCells = [];
   var selectedXCells = [];
-  var winnerDict = new Object();
-  winnerDict = {
-    "0, 1, 2": "winner"
-    , "3, 4, 5": "winner"
-    , "6, 7, 8": "winner"
-    , "0, 3 ,6": "winner"
-    , "1, 4, 7": "winner"
-    , "2, 5, 8": "winner"
-    , "0, 4, 8": "winner"
-    , "2, 4, 6": "winner"
-  };
+  var winnerPositions = [];
+  winnerPositions = [
+    { "pos1": "0", "pos2": "1", "pos3": "2" }
+    , { "pos1": "3", "pos2": "4", "pos3": "5" }
+    , { "pos1": "6", "pos2": "7", "pos3": "8" }
+    , { "pos1": "0", "pos2": "3", "pos3": "6" }
+    , { "pos1": "1", "pos2": "4", "pos3": "7" }
+    , { "pos1": "2", "pos2": "5", "pos3": "8" }
+    , { "pos1": "0", "pos2": "4", "pos3": "8" }
+    , { "pos1": "2", "pos2": "4", "pos3": "6" }
+  ];
 
   document.querySelectorAll('[data-cell]').forEach(function (cell) {
     cell.addEventListener('click', addObject);
@@ -26,34 +26,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelectorAll('[data-cell]').forEach(function (cell) {
       cell.innerHTML = "";
     })
-
     style = 'o';
+    document.querySelector('#announce-winner').innerHTML = "";
   }
 
   function addObject() {
     if (!this.firstChild) {
       this.insertAdjacentHTML('afterbegin', `<div class="disc ${style}">${style}</div>`);
+      var checkedCells = 0;
 
       if (style === 'o') {
         selectedOCells.push(this.getAttribute("data-cell"));
+        checkedCells = selectedOCells.length;
       } else if (style === 'x') {
         selectedXCells.push(this.getAttribute("data-cell"));
+        checkedCells = selectedXCells.length;
       }
 
-      checkWinner(this);
+      if (checkedCells > 2)
+        checkWinner(this);
 
       style = (style === 'o') ? 'x' : 'o';
     }
   }
 
   function checkWinner(clickedCell) {
-    console.log('checkWinner = ' + clickedCell);
     var clickedValue = clickedCell.getAttribute("data-cell");
-    var keys = Object.keys(winnerDict);
-    console.log('keys = ' + keys);
 
-    var matchingWinnerKeys = keys.filter(function (key) { return key.indexOf(clickedValue) > -1 });
-    console.log('matchingWinnerKeys = ' + matchingWinnerKeys);
+    var filteredWinnerPos = winnerPositions.filter(function (winnerPos) {
+      return clickedValue === winnerPos.pos1 || clickedValue === winnerPos.pos2 || clickedValue === winnerPos.pos3;
+    });
 
     var foundWinner = false;
     var selectedValues = selectedOCells;
@@ -61,22 +63,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
       selectedValues = selectedXCells;
     }
 
-    console.log('selectedValues = ' + selectedValues);
-
-    if (selectedValues.length > 2) {
-      for (let i = 0; i < matchingWinnerKeys.length; i++) {
-        if (selectedValues.includes(matchingWinnerKeys[i][0]) & selectedValues.includes(matchingWinnerKeys[i][1]) & selectedValues.includes(matchingWinnerKeys[i][2])) {
-          foundWinner = true;
-        }
+    for (let i = 0; i < filteredWinnerPos.length; i++) {
+      if (selectedValues.includes(filteredWinnerPos[i].pos1) & selectedValues.includes(filteredWinnerPos[i].pos2) & selectedValues.includes(filteredWinnerPos[i].pos3)) {
+        foundWinner = true;
+        break;
       }
     }
-    if (foundWinner === true){
-      endGame;
+    
+    if (foundWinner === true) {
+      endGame();
     }
   }
 
-  function endGame(){
+  function endGame() {
     var announceWinner = document.querySelector("#announce-winner");
-    announceWinner.insertAdjacentHTML('afterbegin', `<div>Congrats ${style} user</div>`);    
+    var capStyle = style.toUpperCase();
+    announceWinner.insertAdjacentHTML('afterbegin', `<div>Congrats ${capStyle} user</div>`);
   }
 });
