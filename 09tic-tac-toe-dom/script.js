13c39c0faefa999f2e20c193a81c940074532ffd
daanishvirani <daanishv@gmail.com>
2018-11-28 18:02:02 -0600

    Added tic-tac-toe initial

---
+++ 09tic-tac-toe-dom/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.querySelectorAll('[data-cell]').forEach(function(cell) {
+    cell.addEventListener('click',function() {
+       )
+    })
+  })
+
+  // You code here
+});
