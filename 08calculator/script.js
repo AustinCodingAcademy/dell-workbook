ef47b1a522c138fe6b52e7372826468d3dad4bb4
danielavillarreal <Daniela_Villarreal@Dell.com>
2018-11-28 19:36:43 -0600

    fixing tab errors for calculator project part 2

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -27,24 +27,24 @@ document.addEventListener("DOMContentLoaded", function(event){
   })
 
   function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
+    document.querySelector("#results").value += num;
+  }
 
   function clearResults() {
-  document.querySelector("#results").value = "";
-}
+    document.querySelector("#results").value = "";
+  }
 
   function equals() {
-  document.querySelector("#results").value = eval(document.querySelector("#results").value);
-}
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+  }
 
   function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
 
   function signSwitchAction(num) {
-  document.querySelector("#results").value *= -1;
-}
+    document.querySelector("#results").value *= -1;
+  }
 
 });
