31c0a6fb725eba4c4876c94c635976287f4e91ca
danielavillarreal <Daniela_Villarreal@Dell.com>
2018-11-28 19:27:12 -0600

    Updated Calculator project

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,13 +1,37 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+  document.querySelectorAll('.button').forEach((button) => {
+
+    button.addEventListener('click', function(event) {
+
+      let text = event.target.innerHTML;
+      if(text === '='){
+        equals();
+      }
+      else if (text === 'Clear'){
+        clearResults();
+      }
+      else if(text === 'Delete'){
+        deleteLast();
+      }
+      else if (text === '+/-'){
+        signSwitchAction();
+      }
+      else{
+        addNumber(text);
+      }
+      
+    })
+  })
+
 function addNumber(num) {
   document.querySelector("#results").value += num;
 }
 
 function clearResults() {
   document.querySelector("#results").value = "";
-}
-
-function addition() {
-  document.querySelector("#results").value += "+";
 }
 
 function equals() {
@@ -19,34 +43,8 @@ function deleteLast() {
   document.querySelector("#results").value = current.slice(0, -1);
 }
 
-function subtractNumber(num){
-  document.querySelector("#results").value -= num;
-}
-
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
-
-function multiplyNumber(num) {
-  document.querySelector("#results").value *= num;
-}
-
-function multiply() {
-  document.querySelector("#results").value += "*";
-}
-
-function divideNumber(num) {
-  document.querySelector("#results").value /= num;
-}
-
-function divide() {
-  document.querySelector("#results").value += "/";
-}
-
-function switchSigns() {
-  document.querySelector("#results").value += "+/-";
-}
-
 function signSwitchAction(num) {
   document.querySelector("#results").value *= -1;
 }
+
+});
