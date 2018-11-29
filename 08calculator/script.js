d9b6dc7a96e65d96ce95295f79d11fbb9fb1e7ce
Devona Spruell <devona_spruell@dell.com>
2018-11-29 08:40:31 -0600

    Updated Calculator with new JS

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,40 +1,38 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', event => {
+  document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', event => {
+      let text = event.target.innerHTML;
+      if (text === 'Enter') {
+        equals();
+      } else if (text === '+/-') {
+        plusneg();
+      } else if (text === 'Clear') {
+        clearResults();
+      } else if (text === 'Delete') {
+        deleteLast();
+      } else {
+        addNumber(text);
+      }
+    });
+  });
+  function equals() {
+    document.querySelector('#results').value = eval(
+      document.querySelector('#results').value
+    );
+  }
+  function deleteLast() {
+    let current = document.querySelector('#results').value;
+    document.querySelector('#results').value = current.slice(0, -1);
+  }
+  function plusneg() {
+    document.querySelector('#results').value *= -1;
+  }
+  function addNumber(num) {
+    document.querySelector('#results').value += num;
+  }
 
-function addNumber(num) {
-  document.querySelector('#results').value += num;
-}
-
-function clearResults() {
-  document.querySelector('#results').value = '';
-}
-
-function decimal() {
-  document.querySelector('#results').value += '.';
-}
-function plusneg() {
-  document.querySelector('#results').value *= -1;
-}
-
-function addition() {
-  document.querySelector('#results').value += '+';
-}
-
-function subtraction() {
-  document.querySelector('#results').value += '-';
-}
-
-function multiplication() {
-  document.querySelector('#results').value += '*';
-}
-function division() {
-  document.querySelector('#results').value += '/';
-}
-function equals() {
-  document.querySelector('#results').value = eval(
-    document.querySelector('#results').value
-  );
-}
-function deleteLast() {
-  let current = document.querySelector('#results').value;
-  document.querySelector('#results').value = current.slice(0, -1);
-}
+  function clearResults() {
+    document.querySelector('#results').value = '';
+  }
+});
