8e2a2f1ef8f63a04bfef60bcee300691e99f4d8d
Stewart <Joseph_C_Stewart@Dell.com>
2018-11-28 19:37:25 -0600

    08calc removed onClick, changed to eventListener

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,42 +1,58 @@
 'use strict';
 
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   // You code here
-// });
-function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+// You code here
+  document.querySelectorAll('.button').forEach((button) => {
+    button.addEventListener('click', function(event) {
+      let text = event.target.innerHTML;
+      if (text === '=') {
+        equals();
+      } else if (text === "Clear") {
+        clearResults();
+      } else if (text === "Delete") {
+        deleteLast();
+      } else if (text === "+/-") {
+        posNeg();
+      } else {
+        addNumber(text)
+      }
+    })
+  })
+  function addNumber(num) {
+    document.querySelector("#results").value += num;
+  }
 
-function clearResults() {
-  document.querySelector("#results").value = "";
-}
+  function clearResults() {
+    document.querySelector("#results").value = "";
+  }
 
-function addition() {
-  document.querySelector("#results").value += "+";
-}
+  function addition() {
+    document.querySelector("#results").value += "+";
+  }
 
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
+  function subtraction() {
+    document.querySelector("#results").value += "-";
+  }
 
-function multiplication() {
-  document.querySelector("#results").value += "*";
-}
+  function multiplication() {
+    document.querySelector("#results").value += "*";
+  }
 
-function division() {
-  document.querySelector("#results").value += "/";
-}
+  function division() {
+    document.querySelector("#results").value += "/";
+  }
 
-function equals() {
-  document.querySelector("#results").value = eval(document.querySelector("#results").value);
-}
+  function equals() {
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+  }
 
-function posNeg() {
-  let changeValue = document.querySelector("#results").value;
-  document.querySelector("#results").value = changeValue * -1;
-}
+  function posNeg() {
+    let changeValue = document.querySelector("#results").value;
+    document.querySelector("#results").value = changeValue * -1;
+  }
 
-function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
+});
