964ea85bfea2513969cf6de3b9c4826ccc88885f
Liu <Fiona_Liu@Dell.com>
2018-11-28 19:15:45 -0600

    calculators tic tac toe dom

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.querySelectorAll('.button').forEach((button) => {
-        GamepadButton.addEventListener('click', function(event) {
-            addNumber(event.target.innerHTML)
-        })
-    })
+  document.querySelectorAll(".button").forEach(button => {
+    GamepadButton.addEventListener("click", function(event) {
+      addNumber(event.target.innerHTML);
+    });
+  });
 });
