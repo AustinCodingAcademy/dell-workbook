e76dd6b61c70384a93f55eca27a34f198a71ef92
Liu <Fiona_Liu@Dell.com>
2018-11-28 19:06:17 -0600

    commit

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -16,6 +16,17 @@ function addition() {
   document.querySelector("#results").value += "+";
 }
 
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function multiplication() {
+  document.querySelector("results").value += "*";
+}
+
+function division() {
+  document.querySelector("#results").value += '/';
+}
 function equals() {
   document.querySelector("#results").value = eval(document.querySelector("#results").value);
 }
