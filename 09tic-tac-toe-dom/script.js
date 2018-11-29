78c4779b8cced004e2610ac7fb327f27e457b411
Marloun Bayari <mbayari@gmail.com>
2018-11-28 20:31:38 -0600

    tictactoe javascript

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,5 +1,13 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function(event){
-
+document.addEventListener("DocContentLoaded", (event) => {
+  let player = 'X';
+  document.querySelectorAll('[data-cell]').forEach(cell => {
+    cell.addEventListener('click', (event) => {
+      event.target.innerHTML = player;
+      player = (player === 'X') ? 'O' : 'X';
+      
+    })
+  })
+  
 });
