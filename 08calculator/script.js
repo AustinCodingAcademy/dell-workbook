a9fe71df5b6ebdd276c470eba036d85c046f2a0d
Todd <Stephen_Todd@dell.com>
2018-11-30 11:37:27 -0600

    New Caculator changes and Tic Tac Toe attempt

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,48 +1,66 @@
 "use strict";
 
-function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
+document.addEventListener("DOMContentLoaded", event => {
+  document.querySelectorAll(".button").forEach(button => {
+    button.addEventListener("click", event => {
+      let text = event.target.innerHTML;
+      if (text === "=") {
+        equals();
+      } else if (text === "Clear") {
+        clearResults();
+      } else if (text === "Delete") {
+        deleteLast();
+      } else if (text === "+/-") {
+        changesign();
+      } else {
+        addNumber(text);
+      }
+    });
+  });
 
-function clearResults() {
-  document.querySelector("#results").value = "";
-}
-
-function addition() {
-  document.querySelector("#results").value += "+";
-}
-
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
-
-function divide() {
-  document.querySelector("#results").value += "/";
-}
-
-function multuply() {
-  document.querySelector("#results").value += "*";
-}
-
-function changesign() {
-  console.log(document.querySelector("#results").value);
-
-  if (document.querySelector("#results").value > 0) {
-    document.querySelector("#results").value =
-      "-" + document.querySelector("#results").value;
-  } else {
-    document.querySelector("#results").value =
-      "+" + document.querySelector("#results").value;
+  function addNumber(num) {
+    document.querySelector("#results").value += num;
   }
-}
 
-function equals() {
-  document.querySelector("#results").value = eval(
-    document.querySelector("#results").value
-  );
-}
+  function clearResults() {
+    document.querySelector("#results").value = "";
+  }
 
-function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
+  function changesign() {
+    // console.log(document.querySelector("#results").value);
+
+    if (document.querySelector("#results").value > 0) {
+      document.querySelector("#results").value =
+        "-" + document.querySelector("#results").value;
+    } else {
+      document.querySelector("#results").value =
+        "+" + document.querySelector("#results").value;
+    }
+  }
+
+  function equals() {
+    document.querySelector("#results").value = eval(
+      document.querySelector("#results").value
+    );
+  }
+
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
+});
+// function addition() {
+//   document.querySelector("#results").value += "+";
+// }
+
+// function subtraction() {
+//   document.querySelector("#results").value += "-";
+// }
+
+// function divide() {
+//   document.querySelector("#results").value += "/";
+// }
+
+// function multuply() {
+//   document.querySelector("#results").value += "*";
+// }
--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,2 +1,26 @@
 "use strict";
-document.addEventListener("DOMContenteLoaded", function(event) {});
+
+document.addEventListener("DOMContentLoaded", event => {
+  let player = "X";
+  document.querySelectorAll("[data-cell]").forEach(cell => {
+    cell.addEventListener("click", event => {
+      event.target.innerHTML = player;
+      player = player === "X" ? "O" : "X";
+    });
+  });
+  function checkForWinner() {
+    // document.querySelector('[data-cell="5"]').innerHTML
+    // 0,1,2 = winner
+    // 3,4,5 = winner
+    // 6,7,8 = winner
+    // 0,3,6 = winner
+    // 1,4,7 = winner
+    // 2,5,8 = winner
+    // 0,4,8 = winner
+    // 6,4,2 = winner
+
+    for (x = 0; x < 8; x++) {
+      document.querySelector('[data-cell="$x"]').innerHTML;
+    }
+  }
+});
