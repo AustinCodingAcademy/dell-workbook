84e8a9a90856b7ec16e80a6f3bb168cf665f0d6c
Hardwick <Endrica_Hardwick@Dell.com>
2018-11-30 13:14:45 -0600

    TicTacToe JS

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -4,15 +4,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let player = 'X';
   document.querySelectorAll('[data-cell]').forEach(cell => {
     cell.addEventListener('click', (event) => {
-        event.target.innerHTML = player;
-        player = (player === 'X') ? 'O' : 'X';
-        if ('[data-cell]' === 'Clear Board') {
-        clearBoard();
-        })
+      event.target.innerHTML = player;
+      player = (player === 'X') ? 'O' : 'X';
     })
   })
-
-  function clearBoard() {
-    document.querySelector('[data-cell]').value = "";
-  }
 });
