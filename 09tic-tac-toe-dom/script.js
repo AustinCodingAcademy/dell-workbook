e45628e265bf1b919a1f768ec6d2b5402035e5d7
Marloun Bayari <mbayari@gmail.com>
2018-11-30 15:14:31 -0600

    another 05grid/index.html fix

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -4,10 +4,58 @@ document.addEventListener("DocContentLoaded", (event) => {
   let player = 'X';
   document.querySelectorAll('[data-cell]').forEach(cell => {
     cell.addEventListener('click', (event) => {
-      event.target.innerHTML = player;
-      player = (player === 'X') ? 'O' : 'X';
+      if (!event.target.innerHTML){
+        event.target.innerHTML = player;
+        checkforWin();
+        player = (player === 'X') ? 'O' : 'X';
+      }
       
-    })
-  })
+      
+    });
+  });
+
+  function checkforWin(){
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ]
+
+    let didPlayerWin = false;
+
+    winningCombos.forEach((combo) => {
+      didPlayerWin = (didPlayerWin || (
+        document.querySelector ('[data-dell="${combo[0]}"]').innerHTML === player &&
+        document.querySelector ('[data-dell="${combo[1]}"]').innerHTML === player &&
+        document.querySelector ('[data-dell="${combo[2]}"]').innerHTML === player
+      ));
+    });
+
+
+    /* -- older brute force way
+    if(
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
+
+
+    )*/
+    if (didPlayerWin) {
+      console.log('Player ${player} Wins!' );
+      return $true;
+    }
+    return false;
+    
+  }
   
 });
