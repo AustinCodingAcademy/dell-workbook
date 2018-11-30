f6121a833c9eb29e4bfe189f508c18c2aaa570fb
Marloun Bayari <mbayari@gmail.com>
2018-11-30 14:34:14 -0600

    07Javascripting/*.js changes

--- 07javascripting/javascripting/for-loop.js
+++ 07javascripting/javascripting/for-loop.js
@@ -2,8 +2,8 @@ var total=0;
 var limit=10;
 
 for (var i=0; i<limit; i++) {
-    total +=i;
-    }
+  total +=i;
+}
 
-    console.log(total);
+console.log(total);
     
--- 07javascripting/javascripting/looping-through-arrays.js
+++ 07javascripting/javascripting/looping-through-arrays.js
@@ -1,6 +1,6 @@
 var pets = ['cat', 'dog', 'rat']
 
 for (var i=0; i<3; i++){
-    pets[i] = pets[i] + 's';
+  pets[i] = pets[i] + 's';
 };
 console.log(pets);
