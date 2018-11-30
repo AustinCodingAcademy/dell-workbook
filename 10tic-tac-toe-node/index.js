f2f071f1d6ef6f2ed1f9d11f418350cb1d05422e
Burns <Catherine_Burns@Dell.com>
2018-11-30 16:51:48 -0600

    Tic Tac Toe Node

--- 10tic-tac-toe-node/index.js
+++ 10tic-tac-toe-node/index.js
@@ -13,6 +13,13 @@ let board = [
 ];
 
 let playerTurn = 'X';
+// let counter = 0;
+// console.log("counter = ",counter);
+// if (counter % 2 === 0 ){
+//   playerTurn = 'X';
+// } else {
+//   playerTurn = 'O';
+// }
 
 function printBoard() {
   console.log('   0  1  2');
@@ -25,21 +32,60 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+  var row;
+  for (row=0;row<3;row++){
+    if (board[row][0]==='X' && board[row][1] ==='X' && board[row][2] ==='X'){
+      return true;
+    } else if (board[row][0]==='O' && board[row][1] ==='O' && board[row][2] ==='O'){
+      return true;
+    }
+  }
+  return false;
+
 }
 
 function verticalWin() {
   // Your code here
+  board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
+  var col;
+  for (col=0;col<3;col++){
+    if (board[0][col]==='X' && board[1][col] ==='X' && board[2][col] ==='X'){
+      return true;
+    } else if (board[0][col]==='O' && board[1][col]==='O' && board[2][col]==='O'){
+      return true;
+    }
+  }
+  return false;
 }
 
 function diagonalWin() {
   // Your code here
+  if (board[0][0]==='X' && board[1][1] ==='X' && board[2][2] ==='X'){
+    return true;
+  } else if (board[0][2]==='X' && board[1][1] ==='X' && board[2][0] ==='X'){
+    return true;
+  } else if (board[0][0]==='O' && board[1][1] ==='O' && board[2][2] ==='O') {
+    return true;
+  } else if (board[0][2]==='O' && board[1][1] ==='O' && board[2][0] ==='O') {
+    return true;
+  }
+  return false;
 }
 
 function checkForWin() {
   // Your code here
+  let win = (horizontalWin()|verticalWin()|diagonalWin()) ? true : false;
+  return win;
 }
 
 function ticTacToe(row, column) {
+  console.log('*****row: ', row, 'column:', column );
+  board[row][column] = playerTurn;
+  if (playerTurn = 'X') {
+    playerTurn = 'O';
+  } else {
+    playerTurn = 'X';
+  }
   // Your code here
 }
 
