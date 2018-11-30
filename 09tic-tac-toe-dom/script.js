5bb44c10e03042db8099c292cb7745e5366194f6
Desangles <Tameesha_Desangles@Dell.com>
2018-11-30 17:02:14 -0600

    i hate javascript

---
+++ 09tic-tac-toe-dom/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  var turn = 'x';
+  document.querySelectorAll('[data-cell]').forEach((cell) => {
+    cell.addEventListener('click', (event) => {
+      event.target.innerHTML = turn;
+      turn = (turn === 'x') ? 'o' : 'x';
+    });
+  })
+});
