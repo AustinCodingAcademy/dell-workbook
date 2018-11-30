9bbb430307d122671dcddc3638adc39fe0624b70
stubclan <aryal.sujan8@gmail.com>
2018-11-30 12:37:50 -0600

    Added calculator project

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,5 +1,46 @@
-'use strict';
+"use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  document.querySelectorAll(".button").forEach(button => {
+    button.addEventListener("click", function(event) {
+      let number = event.target.innerHTML;
+
+      if (number === "=") {
+        equals();
+      } else if (number === "Clear") {
+        clearResults();
+      } else if (number === "Delete") {
+        deleteLast();
+      } else if (number === "+/-") {
+        changeSign();
+      } else {
+        addNumber(number);
+      }
+    });
+  });
 });
+
+function addNumber(num) {
+  document.querySelector("#result").value += num;
+}
+
+function equals() {
+  document.querySelector("#result").value = eval(
+    document.querySelector("#result").value
+  );
+}
+
+function clearResults() {
+  document.querySelector("#result").value = "";
+}
+
+function deleteLast() {
+  let current = document.querySelector("#result").value;
+  document.querySelector("#result").value = current.slice(0, -1);
+}
+
+function changeSign() {
+  let current = document.querySelector("#result").value;
+  var lastNumber = current.slice(0, -1);
+  document.querySelector("#result").value;
+}
