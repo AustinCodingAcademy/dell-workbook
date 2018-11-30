867fc994f8687756775d8735c884a8356469d1c3
Hardwick <Endrica_Hardwick@Dell.com>
2018-11-30 13:06:07 -0600

    Calculator JS format

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -18,41 +18,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
     })
   })
 
-function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
+  function addNumber(num) {
+    document.querySelector("#results").value += num;
+  }
 
-function clearResults() {
-  document.querySelector("#results").value = "";
-}
+  function clearResults() {
+    document.querySelector("#results").value = "";
+  }
 
-function addition() {
-  document.querySelector("#results").value += "+";
-}
+  function addition() {
+    document.querySelector("#results").value += "+";
+  }
 
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
+  function subtraction() {
+    document.querySelector("#results").value += "-";
+  }
 
-function multiplication() {
-  document.querySelector("#results").value += "*";
-}
+  function multiplication() {
+    document.querySelector("#results").value += "*";
+  }
 
-function division() {
-  document.querySelector("#results").value += "/";
-}
+  function division() {
+    document.querySelector("#results").value += "/";
+  }
 
-function negative() {
-  document.querySelector("#results").value = parseFloat(document.querySelector("#results").value) * -1;
-}
+  function negative() {
+    document.querySelector("#results").value = parseFloat(document.querySelector("#results").value) * -1;
+  }
 
-function equals() {
-  document.querySelector("#results").value = eval(document.querySelector("#results").value);
-}
+  function equals() {
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+  }
 
-function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
 
 });
