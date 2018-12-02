62b7295ddd5a6f6ea317049c61949443dda512a1
STodd1119 <stephen_todd@dell.com>
2018-12-02 11:50:12 -0600

    Mods to 09 tic-tac-toe for clearing board that were not added at last submission

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -11,13 +11,11 @@ document.addEventListener("DOMContentLoaded", event => {
   });
   clear.addEventListener("click", event => {
     //for each cell clear the contents and set player back to X
-    let player = "X"
+    let player = "X";
     for (var x = 0; x <= 8; x++) {
       document.querySelector(`[data-cell="${x}"]`).innerHTML = " ";
     }
-    
-  })
-
+  });
 
   function checkForWinner() {
     // 0,1,2 = winner
@@ -29,21 +27,34 @@ document.addEventListener("DOMContentLoaded", event => {
     // 0,4,8 = winner
     // 6,4,2 = winner
     if (
-    ((document.querySelector('[data-cell="0"]').innerHTML === player) && (document.querySelector('[data-cell="1"]').innerHTML === player) && (document.querySelector('[data-cell="2"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="3"]').innerHTML === player) && (document.querySelector('[data-cell="4"]').innerHTML === player) && (document.querySelector('[data-cell="5"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="6"]').innerHTML === player) && (document.querySelector('[data-cell="7"]').innerHTML === player) && (document.querySelector('[data-cell="8"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="0"]').innerHTML === player) && (document.querySelector('[data-cell="3"]').innerHTML === player) && (document.querySelector('[data-cell="6"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="1"]').innerHTML === player) && (document.querySelector('[data-cell="4"]').innerHTML === player) && (document.querySelector('[data-cell="7"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="2"]').innerHTML === player) && (document.querySelector('[data-cell="5"]').innerHTML === player) && (document.querySelector('[data-cell="8"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="0"]').innerHTML === player) && (document.querySelector('[data-cell="4"]').innerHTML === player) && (document.querySelector('[data-cell="8"]').innerHTML === player)) ||
-    ((document.querySelector('[data-cell="6"]').innerHTML === player) && (document.querySelector('[data-cell="4"]').innerHTML === player) && (document.querySelector('[data-cell="2"]').innerHTML === player)) 
-      ) {
-        console.log(`Player ${player} Wins!`);
-        return true;
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="1"]').innerHTML === player &&
+        document.querySelector('[data-cell="2"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="3"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML === player &&
+        document.querySelector('[data-cell="5"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="6"]').innerHTML === player &&
+        document.querySelector('[data-cell="7"]').innerHTML === player &&
+        document.querySelector('[data-cell="8"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="3"]').innerHTML === player &&
+        document.querySelector('[data-cell="6"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="1"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML === player &&
+        document.querySelector('[data-cell="7"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="2"]').innerHTML === player &&
+        document.querySelector('[data-cell="5"]').innerHTML === player &&
+        document.querySelector('[data-cell="8"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML === player &&
+        document.querySelector('[data-cell="8"]').innerHTML === player) ||
+      (document.querySelector('[data-cell="6"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML === player &&
+        document.querySelector('[data-cell="2"]').innerHTML === player)
+    ) {
+      console.log(`Player ${player} Wins!`);
+      return true;
     }
     return false;
   }
-
-
-
 });
