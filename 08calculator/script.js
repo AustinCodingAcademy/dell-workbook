2839d7ee6a9620853a1ed05a31f73ee30a945907
Patti Parnell <Patricia_Parnell@dell.com>
2018-12-02 20:17:02 -0600

    calc, tic tac toe v1

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -1,44 +1,54 @@
 "use strict";
-function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
 
-function clearResults() {
-  document.querySelector("#results").value = "";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".button").forEach(button =>
+    button.addEventListener("click", event => {
+      let text = event.target.innerHTML;
+      switch (text) {
+        case "=":
+          equals();
+          break;
+        case "Delete":
+          deleteLast();
+          break;
+        case "Clear":
+          clearResults();
+          break;
+        case "+/-":
+          changeSign();
+          break;
+        default:
+          addNumber(text);
+          break;
+      }
+    })
+  );
 
-function addition() {
-  document.querySelector("#results").value += "+";
-}
-
-function equals() {
-  if (document.querySelector("#results").value !== "") {
-    document.querySelector("#results").value = eval(
-      document.querySelector("#results").value
-    );
+  function addNumber(num) {
+    document.querySelector("#results").value += num;
   }
-}
 
-function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
-
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
-
-function multiple() {
-  document.querySelector("#results").value += "*";
-}
-
-function divide() {
-  document.querySelector("#results").value += "/";
-}
-
-function changeSign() {
-  if (!isNaN(Number(document.querySelector("#results").value))) {
-    document.querySelector("#results").value =
-      eval(document.querySelector("#results").value) * -1;
+  function clearResults() {
+    document.querySelector("#results").value = "";
   }
-}
+
+  function equals() {
+    if (document.querySelector("#results").value !== "") {
+      document.querySelector("#results").value = eval(
+        document.querySelector("#results").value
+      );
+    }
+  }
+
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
+
+  function changeSign() {
+    if (!isNaN(Number(document.querySelector("#results").value))) {
+      document.querySelector("#results").value =
+        eval(document.querySelector("#results").value) * -1;
+    }
+  }
+});
---
+++ 09tic-tac-toe-dom/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  let player = "X";
+
+  document.querySelectorAll("[data-cell]").forEach(cell => {
+    cell.addEventListener("click", event => {
+      changePlayer();
+
+      if (cell.innerHTML === ""){
+        cell.innerHTML = player;
+    } //only allows empty cell to be updated
+    });
+
+    checkWin ();
+  });
+
+  // !--toggle between x and
+  // clear board
+  // after each play - check for a win
+  //dont overwrite
+
+  function changePlayer() {
+    if (player === "X") {
+      player = "O";
+    } else {
+      player = "X";
+    }
+  }
+
+  function checkWin (){
+
+    // switch (key) {
+    //   case value:
+        
+    //     break;
+    
+    //   default:
+    //     break;
+    // }
+    // if ([data-cell]="0".innerHTML === [data-cell]="1".innerHTML ) &&  ([data-cell]="1".innerHTML === [data-cell]="2".innerHTML ))
+    //   console.log ('win')
+    // }
+    
+  }
+  
+});
