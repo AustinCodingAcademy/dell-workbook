65752fbff53019f9f1028a40d43c4559fd9d9dea
Burns <Catherine_Burns@Dell.com>
2018-12-03 11:48:11 -0600

    Updating calc

--- 08calculator/script.js
+++ 08calculator/script.js
@@ -25,39 +25,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     })
   })
+
+
+  function addNumber(num) {
+    document.querySelector("#results").value += num;
+  }
+  
+  function clearResults() {
+    document.querySelector("#results").value = "";
+  }
+  
+  function addition() {
+    document.querySelector("#results").value += "+";
+  }
+  function subtraction() {
+    document.querySelector("#results").value += "-";
+  }
+  function multiply() {
+    document.querySelector("#results").value += "*";
+  }
+  function divide() {
+    document.querySelector("#results").value += "/";
+  }
+  function plusminus() {
+    var mynum = document.querySelector("#results").value 
+    var newnum = mynum*(-1); 
+    console.log(newnum)
+    document.querySelector("#results").value = newnum;
+  }
+  function equals() {
+    document.querySelector("#results").value = eval(document.querySelector("#results").value);
+  }
+  
+  function deleteLast() {
+    let current = document.querySelector("#results").value;
+    document.querySelector("#results").value = current.slice(0, -1);
+  }
 });
 
-function addNumber(num) {
-  document.querySelector("#results").value += num;
-}
-
-function clearResults() {
-  document.querySelector("#results").value = "";
-}
-
-function addition() {
-  document.querySelector("#results").value += "+";
-}
-function subtraction() {
-  document.querySelector("#results").value += "-";
-}
-function multiply() {
-  document.querySelector("#results").value += "*";
-}
-function divide() {
-  document.querySelector("#results").value += "/";
-}
-function plusminus() {
-  var mynum = document.querySelector("#results").value 
-  var newnum = mynum*(-1); 
-  console.log(newnum)
-  document.querySelector("#results").value = newnum;
-}
-function equals() {
-  document.querySelector("#results").value = eval(document.querySelector("#results").value);
-}
-
-function deleteLast() {
-  let current = document.querySelector("#results").value;
-  document.querySelector("#results").value = current.slice(0, -1);
-}
