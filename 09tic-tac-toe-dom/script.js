0d42313bb8ad856cf7ed1e4ea882d79c66c0405d
Marloun Bayari <mbayari@gmail.com>
2018-12-05 13:27:26 -0600

    uploaded tic-tac-toe-dom

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,61 +1,48 @@
-'use strict';
+"use strict";
 
-document.addEventListener("DocContentLoaded", (event) => {
-  let player = 'X';
-  document.querySelectorAll('[data-cell]').forEach(cell => {
-    cell.addEventListener('click', (event) => {
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Your Code Here
+  let player = "X";
+  document.querySelectorAll("[data-cell]").forEach(cell => {
+    cell.addEventListener("click", event => {
       if (!event.target.innerHTML) {
         event.target.innerHTML = player;
-        checkforWin();
-        player = (player === 'X') ? 'O' : 'X';
+        checkForWin();
+        player = player === "X" ? "O" : "X";
       }
-
-
     });
   });
 
-  function checkforWin() {
-    const winningCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [6, 4, 2]
-    ]
-
-    let didPlayerWin = false;
-
-    winningCombos.forEach((combo) => {
-      didPlayerWin = (didPlayerWin || (
-        document.querySelector('[data-dell="${combo[0]}"]').innerHTML === player &&
-        document.querySelector('[data-dell="${combo[1]}"]').innerHTML === player &&
-        document.querySelector('[data-dell="${combo[2]}"]').innerHTML === player
-      ));
-    });
-
-
-    /* -- older brute force way use to turn in assignment
-    if(
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-      (document.querySelector('[data-cell="0"]') === player.innerHTML && document.querySelector('[data-cell="1"]') === player.innerHTML && document.querySelector('[data-cell="0"]') === player.innerHTML) ||
-
-
-    )*/
-    if (didPlayerWin) {
-      console.log('Player ${player} Wins!');
-      return $true;
+  function checkForWin() {
+    if (
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="1"]').innerHTML &&
+        document.querySelector('[data-cell="2"]').innerHTML) ||
+      (document.querySelector('[data-cell="3"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML &&
+        document.querySelector('[data-cell="5"]').innerHTML) ||
+      (document.querySelector('[data-cell="6"]').innerHTML === player &&
+        document.querySelector('[data-cell="7"]').innerHTML &&
+        document.querySelector('[data-cell="8"]').innerHTML) ||
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="3"]').innerHTML &&
+        document.querySelector('[data-cell="6"]').innerHTML) ||
+      (document.querySelector('[data-cell="1"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML &&
+        document.querySelector('[data-cell="7"]').innerHTML) ||
+      (document.querySelector('[data-cell="2"]').innerHTML === player &&
+        document.querySelector('[data-cell="5"]').innerHTML &&
+        document.querySelector('[data-cell="8"]').innerHTML) ||
+      (document.querySelector('[data-cell="0"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML &&
+        document.querySelector('[data-cell="8"]').innerHTML) ||
+      (document.querySelector('[data-cell="6"]').innerHTML === player &&
+        document.querySelector('[data-cell="4"]').innerHTML &&
+        document.querySelector('[data-cell="2"]').innerHTML)
+    ) {
+      console.log(`Player ${player} Wins!`);
+      return true;
     }
     return false;
-
   }
-
 });
