48caa8dc9d625548d64c195cec7ea9b8b2e6eb73
Puvadi <Anil_Kumar_Puvadi@Dell.com>
2018-12-06 11:54:28 -0600

    tower of hanoi

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -5,31 +5,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   document.querySelectorAll("[data-cell]").forEach(cell => {
     cell.addEventListener("click", event => {
-      if (player === "X") {
+      if (player === "X" && event.target.innerHTML === "") {
         event.target.innerHTML = "X";
         player = "Y";
-      } else if (player === "Y") {
+      } else if (player === "Y" && event.target.innerHTML === "") {
         event.target.innerHTML = "O";
         player = "X";
       }
-      // if (player === "Z") {
-      //   player = "Y";
-      // }
     });
   });
 
-  //   document.querySelector("#clear") => {
-  //       clear.addEventListener("click",event => {
-  //         document.querySelectorAll("[data-cell]").forEach(cell => {
-  //             event.target.innerHTML = ""  ;
-  //         }
-  //       })
-  //   }
+  document.querySelector('#clear').addEventListener('click', function() {
+    document.querySelectorAll("[data-cell]").forEach(cell => {
+      cell.innerHTML = "";
+    });
+  })
 });
-function myFunction() {
-  //document.querySelectorAll("[data-cell]").forEach(cell => {
-  let p = document.querySelectorAll("[data-cell]");
-  for (let i = 0; i < p.length; i++) {
-    p[i].innerHTML = "";
-  }
-}
