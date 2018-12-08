f6f3a6ff008f270d03ba9a24ec08d5c2b7545e3d
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-12-07 22:16:38 -0600

    Added hw #11 and #12

--- 11towers-of-hanoi-dom/script.js
+++ 11towers-of-hanoi-dom/script.js
@@ -1,5 +1,37 @@
 'use strict';
 
+// console.log(this.getAttribute("data-stack"));
+      // console.log(this.querySelectorAll("[data-block]"));
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  // You code here
+  let playerTurn = '1';
+  let beginStack;
+  let endStack;
+
+  document.querySelectorAll("[data-stack]").forEach(cell => cell.addEventListener('click', selectObject));
+
+  function selectObject(){
+    if(playerTurn === '1'){
+      beginStack = this;
+    }else if(playerTurn === '2'){
+      endStack = this;
+      if(beginStack !== '')
+        movePiece()
+    }
+
+    playerTurn = playerTurn === '1' ? '2' : '1'; 
+  }
+
+  function movePiece(){
+    let beginStackElements = beginStack.querySelectorAll("[data-block");
+    endStack.appendChild(beginStackElements[beginStackElements.length - 1]);
+  }
+
+  function isLegal(){
+    let moveElement = beginStack.querySelectorAll("[data-block");
+    
+  }
 });
+
+
+//Const in javascript/c#
--- 12algorithms/index.js
+++ 12algorithms/index.js
@@ -15,11 +15,47 @@ for (let i = 0; i < 1000; i++) {
 }
 
 function bubbleSort(arr) {
-  // Your code here
+  let sort = true;
+
+  while(sort){
+    sort = false;
+    for(let i = 0; i < arr.length; i++ ){
+      if(arr[i] > arr[i+1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        sort = true;
+      }
+    }
+  }
+
+  return arr;
 }
 
 function mergeSort(arr) {
-  // Your code here
+  if(arr.length < 2){
+    return arr;
+  }
+
+  const midIdx = arr.length / 2;
+  const arr1 = mergeSort(arr.slice(0, midIdx));
+  const arr2 = mergeSort(arr.slice(midIdx));
+
+  const sortedArr = [];
+  while(arr1.length && arr2.length){
+    if(arr1[0] < arr2[0]){
+      sortedArr.push(arr1.shift());
+    } else {
+      sortedArr.push(arr2.shift());
+    }
+  }
+
+  if(arr1.length)
+  {
+    sortedArr = sortedArr.concat(arr1);
+  }
+
+  return sortedArr;
 }
 
 function binarySearch(arr, item) {
