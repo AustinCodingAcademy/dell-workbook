66510cf26e40442c3418391da1f702f262e7edf9
Hardwick <Endrica_Hardwick@Dell.com>
2018-12-09 12:44:47 -0600

    ticktactoenode format fix

--- 10tic-tac-toe-node/index.js
+++ 10tic-tac-toe-node/index.js
@@ -35,15 +35,9 @@ function horizontalWin() {
 function verticalWin() {
   // Your code here
   if (
-    (board[0][0] === board[1][0] &&
-      board[0][0] === board[2][0] &&
-      board[0][0] === playerTurn) ||
-    (board[0][1] === board[1][1] &&
-      board[1][1] === board[2][1] &&
-      board[0][1] === playerTurn) ||
-    (board[0][2] === board[1][2] &&
-      board[1][2] === board[2][2] &&
-      board[0][2] === playerTurn)
+    (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] === playerTurn) ||
+    (board[0][1] === board[1][1] && board[1][1] === board[2][1] && board[0][1] === playerTurn) ||
+    (board[0][2] === board[1][2] && board[1][2] === board[2][2] && board[0][2] === playerTurn)
   ) {
     return true;
   } else {
