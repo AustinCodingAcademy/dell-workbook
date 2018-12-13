68b1e94f462209f5b5a6f869776efa9ef7c2092d
Liu <Fiona_Liu@Dell.com>
2018-12-12 20:19:53 -0600

    nice

--- 14final/script.js
+++ 14final/script.js
@@ -3,23 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
 var count = 0;
     document.querySelector('button').addEventListener('click', () => {
 
-
-        const currentSpeed = document.querySelector('#slowSpeed').style.animationDuration;
+        var currentSpeed = document.querySelector('#slowSpeed').style.animationDuration;
         document.querySelector('img').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 2}s`;
         count = count - 10;
         document.querySelector('#count').innerHTML = 'Shaq has had ' + count + ' drinkos';
 
-        const currentSpeed = document.querySelector('#mediumSpeed').style.animationDuration;
+        
+
+    })
+    document.querySelector('button').addEventListener('click', () => {
+    currentSpeed = document.querySelector('#mediumSpeed').style.animationDuration;
         document.querySelector('img').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 2}s`;
         count = count - 100;
         document.querySelector('#count').innerHTML = 'Shaq has had ' + count + ' drinkos';
 
+    })
 
-        const currentSpeed = document.querySelector('#mediumSpeed').style.animationDuration;
+    document.querySelector('button').addEventListener('click', () => {
+        currentSpeed = document.querySelector('superSpeed').style.animationDuration;
         document.querySelector('img').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 2}s`;
         count = count - 1000;
         document.querySelector('#count').innerHTML = 'Shaq has had ' + count + ' drinkos';
-
     })
 
     document.querySelector('img').addEventListener('click', () => {
