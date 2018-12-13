a9bcd13c1d6808eb92024114fd7d4e8ca0c679f4
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-12-12 21:47:45 -0600

    Added binary searh method and also fixed the mergeSort issues

--- 12algorithms/index.js
+++ 12algorithms/index.js
@@ -17,13 +17,13 @@ for (let i = 0; i < 1000; i++) {
 function bubbleSort(arr) {
   let sort = true;
 
-  while(sort){
+  while (sort) {
     sort = false;
-    for(let i = 0; i < arr.length; i++ ){
-      if(arr[i] > arr[i+1]){
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
         let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
         sort = true;
       }
     }
@@ -33,33 +33,52 @@ function bubbleSort(arr) {
 }
 
 function mergeSort(arr) {
-  if(arr.length < 2){
+  if (arr.length < 2) {
     return arr;
   }
 
-  const midIdx = arr.length / 2;
+  const midIdx = Math.floor(arr.length / 2);
   const arr1 = mergeSort(arr.slice(0, midIdx));
   const arr2 = mergeSort(arr.slice(midIdx));
 
   const sortedArr = [];
-  while(arr1.length && arr2.length){
-    if(arr1[0] < arr2[0]){
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
       sortedArr.push(arr1.shift());
     } else {
       sortedArr.push(arr2.shift());
     }
   }
 
-  if(arr1.length)
-  {
-    sortedArr = sortedArr.concat(arr1);
+  if (arr1.length) {
+    sortedArr.push.apply(sortedArr, arr1);
+  } else {
+    sortedArr.push.apply(sortedArr, arr2);
   }
 
   return sortedArr;
 }
 
 function binarySearch(arr, item) {
-  // Your code here
+  let targetPos = Math.floor(arr.length / 2);
+  let previousPos = arr.length;
+  let sliceLength = targetPos;
+  
+  while (sliceLength > 0) {
+    if (arr[targetPos] === item) {
+      return targetPos;
+    } else if (arr[targetPos] > item) {
+      previousPos = targetPos;
+      targetPos = Math.floor(targetPos / 2);
+      sliceLength = Math.floor(sliceLength / 2);
+    } else if (arr[targetPos] < item) {
+      previousPos = previousPos > targetPos ? previousPos : targetPos;
+      sliceLength = Math.floor(sliceLength / 2);
+      targetPos = Math.floor((targetPos + previousPos) / 2);
+    }
+  }
+
+  return false;
 }
 
 // Tests
@@ -88,8 +107,8 @@ if (typeof describe === 'function') {
 
   describe('#binarySearch()', () => {
     it('should return the index of given item if sorted array contains it', () => {
-      const idx = binarySearch([1, 2, 3, 4], 3);
-      assert.equal(idx, 2);
+      const idx = binarySearch([0, 1, 2, 3, 4], 3);
+      assert.equal(idx, 3);
     });
     it('should return false if item not in sorted array', () => {
       const idx = binarySearch([1, 2, 3, 4], 5);
