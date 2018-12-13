98898fd37c3b223a721161e022acb7ed08a45dd3
Liu <Fiona_Liu@Dell.com>
2018-12-12 19:58:09 -0600

    shaq spins

--- 14final/script.js
+++ 14final/script.js
@@ -1,9 +1,16 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-
+var count = 0;
     document.querySelector('button').addEventListener('click', () => {
-        const currentSpeed = document.querySelector('.square').style.animationDuration;
-        document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0])+1}s`;
 
+
+        const currentSpeed = document.querySelector('img').style.animationDuration;
+        document.querySelector('img').style.animationDuration = `${Number(currentSpeed.split('s')[0])+1}s`;
+
+    })
+
+    document.querySelector('img').addEventListener('click', () => {
+        count+= 1;
+        document.querySelector('#count').innerHTML = 'Shaq has had ' + count + ' drinkos';
     })
 })
