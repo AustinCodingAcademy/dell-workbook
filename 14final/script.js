fea090be01fa78b83da472bcdc36d6ec8f0654b0
AngelineDai <44105373+AngelineDai@users.noreply.github.com>
2018-12-14 16:23:25 -0600

    Initialed the coding for the final project from class videos

---
+++ 14final/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#slow').addEventListener('click', () => {
+    const currentSpeed = document.querySelector('.square').style.animationDuration;
+    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 1}s`;
+  });
+
+  document.querySelector('#slower').addEventListener('click', () => {
+    const currentSpeed = document.querySelector('.square').style.animationDuration;
+    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 10}s`;
+  });
+
+  // To get current animation speed in DOM
+  // getComputedStyle(document.querySelector('.square')).animationDuration;
+});
