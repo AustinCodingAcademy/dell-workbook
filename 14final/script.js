b0065ab54ceae0198e41c356fef0b9508387af7b
Hernandez <Jonathan_Hernandez2@Dell.com>
2018-12-16 21:37:03 -0600

    final lint fixed

--- 14final/script.js
+++ 14final/script.js
@@ -1,15 +1,19 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  document.querySelector('#slow').addEventListener('click', () => {
-    const currentSpeed = document.querySelector('.square').style.animationDuration;
-    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 1}s`
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#slow").addEventListener("click", () => {
+    const currentSpeed = document.querySelector(".square").style
+      .animationDuration;
+    document.querySelector(".square").style.animationDuration = `${Number(
+      currentSpeed.split("s")[0]
+    ) + 1}s`;
   });
 
-  document.querySelector('#slower').addEventListener('click', () => {
-    const currentSpeed = document.querySelector('.square').style.animationDuration;
-    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 10}s`
+  document.querySelector("#slower").addEventListener("click", () => {
+    const currentSpeed = document.querySelector(".square").style
+      .animationDuration;
+    document.querySelector(".square").style.animationDuration = `${Number(
+      currentSpeed.split("s")[0]
+    ) + 10}s`;
   });
-
 });
