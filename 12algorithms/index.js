41d00207063f1c5e242d1fa745daf4f485d0a3d0
Hardwick <Endrica_Hardwick@Dell.com>
2018-12-19 17:58:06 -0600

    algorithms

--- 12algorithms/index.js
+++ 12algorithms/index.js
@@ -10,21 +10,106 @@ function getRandomInt(min, max) {
 
 let arr = [];
 
-for (let i = 0; i < 1000; i++) {
-  arr.push(getRandomInt(0, 1000));
+for (let i = 0; i < 10; i++) {
+  arr.push(getRandomInt(0, 10));
 }
 
+// arr = [2, 1, 3, 7, 8, 9];
+function swap(firstIdx, secondIdx, arr) {
+  let temp = arr[firstIdx];
+  arr[firstIdx] = arr[secondIdx];
+  arr[secondIdx] = temp;
+}
+
+// Bubble sort is not a good way to sort because it
+// takes n2 number of times items need to be touched
 function bubbleSort(arr) {
-  // Your code here
+  let counter = 0;
+  for (let i = 0; i < arr.length -1; i++) {
+    let sorted = true;
+    for (let j=0; j < arr.length -1; j++) {
+      counter++;
+      console.log(arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        sorted = false;
+        swap(j, j + 1, arr);
+      }
+    }
+    if (sorted) {
+      break;
+    }
+  }
+  console.log(`Took me ${counter} times!`)
+  return arr;
 }
+// console.log(arr);
+// console.log(bubbleSort(arr));
 
+// arr = [4, 5, 7, 8, 9, 0, 9, 1];
+// mergsort splits array in half, sorts, and merges back
 function mergeSort(arr) {
-  // Your code here
-}
+  if (arr.length < 2) {
+    return arr;
+  }
+ 
+  const midIndex = Math.floor(arr.length / 2); // 4
+  const arr1 = mergeSort(arr.slice(0, midIndex));
+  const arr2 = mergeSort(arr.slice(midIndex));
+  let sortedArr = [];
 
-function binarySearch(arr, item) {
-  // Your code here
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      sortedArr.push(arr1.shift());
+    } else {
+      sortedArr.push(arr2.shift());
+    }
+  }
+  if (arr1.length) {
+    sortedArr = [...sortedArr, ...arr1];
+  } else {
+    sortedArr = [...sortedArr, ...arr2];
+  }
+    return sortedArr;
+
+  // if (arr1.length) {
+  //   Array.push.apply(sortedArr, arr1);
+  // } else {
+  //   Array.push.apply(sortedArr, arr2);
+  // }
+  // return sortedArr;
 }
+// console.log(arr);
+// console.log(mergeSort(arr));
+
+// .pop() ==> .shift()
+// .push() ==> .unshift()
+
+// arr = [7, 5, 2, 8, 6, 0, 9, 1];
+// Binary search splits array in to and compares to target item
+function binarySearch(alist, item) {
+  // Your code here
+  let minIndex = 0;
+  let maxIndex = alist.length - 1;
+  let currentIndex;
+  let currentElement;
+  // let item;
+
+  while (minIndex <= maxIndex) {
+    // currentIndex = Math.ceil((minIndex + maxIndex) / 2);
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = alist[currentIndex];
+    if (currentElement === item) {
+        return currentElement;
+    } else if (item > currentElement) {
+      minIndex = currentIndex + 1;
+    } else {
+      maxIndex = currentIndex -1;
+    }
+    // return -1;
+  }
+}
+// console.log(binarySearch(alist));
+// console.log(binarySearch(alist));
 
 // Tests
 
