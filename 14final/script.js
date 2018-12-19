0b2605bb0c362f64c3ef7e70b635e7d00b68c4e0
Patti Parnell <Patricia_Parnell@dell.com>
2018-12-19 15:16:17 -0600

    final

---
+++ 14final/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+let taxes = 0;
+let ruled = 1;
+let warCost = 50 * ruled;
+let jewelCost = 15;
+let jewelsOwned = 1;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("coin").onclick = function(e) {
+    taxes = taxes + 1 * ruled;
+    console.log("taxes " + taxes);
+    document.getElementById("taxescollected").placeholder = "$ " + taxes;
+  };
+  document.querySelector("#crown2").style["display"] = "none";
+});
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("war").onclick = function(e) {
+    console.log("taxes " + taxes);
+    console.log("ruled " + ruled);
+    if (taxes >= warCost) {
+      updateWarCost();
+    }
+  };
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("buy_jewels").onclick = function(e) {
+    if (taxes >= jewelCost) {
+      console.log("taxes " + taxes);
+      console.log("jewelCost " + ruled);
+      updateJewels();
+    }
+  };
+});
+
+function updateJewels() {
+  taxes = taxes - jewelCost;
+  jewelCost = ruled * jewelsOwned * 150;
+  jewelsOwned = jewelsOwned + 1;
+
+  document.querySelector("#crown2").style["display"] = "inline";
+  document.getElementById("taxescollected").placeholder = "$ " + taxes;
+  document.getElementById("kingdomsruled").placeholder = ruled;
+  document.getElementById("jewel_cost").placeholder = "$ " + jewelCost;
+}
+
+function updateWarCost() {
+  ruled = ruled + 1;
+  taxes = taxes - warCost;
+  warCost = ruled * 50;
+  console.log("taxes " + taxes);
+  console.log("ruled " + ruled);
+  document.getElementById("taxescollected").placeholder = "$ " + taxes;
+  document.getElementById("kingdomsruled").placeholder = ruled;
+  document.getElementById("war_cost").placeholder = "$ " + warCost;
+}
