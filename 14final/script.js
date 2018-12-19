cef810083892a7460bdbe78f992f3dbb98796ef1
Puvadi <Anil_Kumar_Puvadi@Dell.com>
2018-12-19 14:43:35 -0600

    Final Project

--- 14final/script.js
+++ 14final/script.js
@@ -3,13 +3,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('#slow').addEventListener('click', () => {
-    const currentSpeed = getComputedStyle(document.querySelector('.square').style.animationDuration);
-    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 1}s`;
+    const currentSpeed = document.querySelector('.square').style.animationDuration;
+    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 1}s`
   });
 
   document.querySelector('#slower').addEventListener('click', () => {
-    const currentSpeed = getComputedStyle(document.querySelector('.square').style.animationDuration);
-    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 10}s`;
+    const currentSpeed = document.querySelector('.square').style.animationDuration;
+    document.querySelector('.square').style.animationDuration = `${Number(currentSpeed.split('s')[0]) + 10}s`
   });
 
 });
