60918845fe5dad29423e8200618f71820f63f851
Golla <Mounika_Golla@Dell.com>
2018-12-21 15:44:51 -0600

    circleci - javascripting02

--- 07javascripting/scope.js
+++ 07javascripting/scope.js
@@ -4,9 +4,9 @@ var a = 1, b = 2, c = 3;
   var b = 5, c = 6;
 
   (function secondFunction() {
-  var b = 8;
-  console.log("a: " + a + ", b: " + b + ", c: " + c);
-  (function thirdFunction() {
+    var b = 8;
+    console.log("a: " + a + ", b: " + b + ", c: " + c);
+    (function thirdFunction() {
       var a = 7, c = 9;
 
       (function fourthFunction() {
