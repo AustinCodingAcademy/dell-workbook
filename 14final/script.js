70668f4065b4b2951c52a821d0fa5fd5d9dff2ec
Lam, Alice <Alice_Lam@Dell.com>
2018-12-21 15:10:54 -0600

    adding in ending

--- 14final/script.js
+++ 14final/script.js
@@ -8,6 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
     if (num < 24) {
       speedUp();
     }
+    if (num == 25){
+      document.querySelector(".chatbubble").style.display='block';
+    }
     document.getElementById("counter").innerHTML =
       "c o o k i e   c o u n t e r : " + num;
     num++;
@@ -15,9 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function speedUp() {
-  const currentSpeed = getComputedStyle(document.querySelector(".cookie"))
-    .animationDuration;
-  console.log("current speed" + currentSpeed);
+  const currentSpeed = getComputedStyle(document.querySelector(".cookie")).animationDuration;
   document.querySelector(".cookie").style.animationDuration = `${Number(
     currentSpeed.split("s")[0]
   ) - 1}s`;
