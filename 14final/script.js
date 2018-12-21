ead8c6cfbaef5b1905de8ec5880c319689dc314c
Lam, Alice <Alice_Lam@Dell.com>
2018-12-21 14:49:28 -0600

    adding speedup function for cookie

--- 14final/script.js
+++ 14final/script.js
@@ -1,15 +1,25 @@
-'use strict';
+"use strict";
 var num = 0;
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("counter").innerHTML = "c o o k i e   c o u n t e r : " + (num);
-    document.querySelector('#speedup').addEventListener('click', () => {
-
-        const currentSpeed = document.querySelector('.cookie').style.animationDuration;
-        console.log(currentSpeed);
-        document.querySelector('.cookie').style.animationDuration = `${Number(currentSpeed.split('s')[0]) - 5}s`
-        document.getElementById("counter").innerHTML = "c o o k i e   c o u n t e r : " + (num);
-        num++;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("counter").innerHTML =
+    "c o o k i e   c o u n t e r : " + num;
+  document.querySelector("#speedup").addEventListener("click", () => {
+    if (num < 24) {
+      speedUp();
+    }
+    document.getElementById("counter").innerHTML =
+      "c o o k i e   c o u n t e r : " + num;
+    num++;
   });
-
 });
+
+function speedUp() {
+  const currentSpeed = getComputedStyle(document.querySelector(".cookie"))
+    .animationDuration;
+  console.log("current speed" + currentSpeed);
+  document.querySelector(".cookie").style.animationDuration = `${Number(
+    currentSpeed.split("s")[0]
+  ) - 1}s`;
+  console.log("current speed" + currentSpeed);
+}
