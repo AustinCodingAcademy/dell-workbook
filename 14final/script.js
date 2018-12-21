f26d461a6608d361689599ac14e2f888fc5ce988
Patti Parnell <Patricia_Parnell@dell.com>
2018-12-21 01:04:13 -0600

    final 8

--- 14final/script.js
+++ 14final/script.js
@@ -1,30 +1,30 @@
 "use strict";
 // initializing my vairables
-let taxes = 0;              //holds taxes collected
-let ruled = 1;              //holds kingdoms rules
-let jewelsOwned = 1;        //holds number of jewels owned
-let warCost = 50;           //holds cost to wage war to win another kingdom
-let jewelCost = 150;        //holds cost to buy next crowns.  
-let startGame = 0;          //used to initialize game when form first opens
-let maxJewels = 30;         //max number of jewels that can be purchased 
-
+let taxes = 0; //holds taxes collected
+let ruled = 1; //holds kingdoms rules
+let jewelsOwned = 1; //holds number of jewels owned
+let warCost = 50; //holds cost to wage war to win another kingdom
+let jewelCost = 150; //holds cost to buy next crowns.
+let startGame = 0; //used to initialize game when form first opens
+let maxJewels = 30; //max number of jewels that can be purchased
+let numberKingdoms = 5; //max number of kingdoms in the world
 
 //listener to form events
 document.addEventListener("DOMContentLoaded", () => {
   if (!startGame) {
     //games hasn't started so calls routine to initialize form
-    initializeForm();  
+    initializeForm();
   }
 
   document.getElementById("coin").onclick = function(e) {
     //updates taxes collected for each click
-    taxes = taxes + (1 * ruled * jewelsOwned);
+    taxes = taxes + 1 * ruled * jewelsOwned;
     console.log("taxes " + taxes);
     updateForm();
   };
 
   document.getElementById("war").onclick = function(e) {
-    //checks first to see if enough funds to wage war 
+    //checks first to see if enough funds to wage war
     //then updates kingdoms rules
 
     console.log("taxes " + taxes);
@@ -48,7 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
     //re-initializes form if user wants to start over
     initializeForm();
   };
-  
 });
 
 function updateJewels() {
@@ -57,7 +56,7 @@ function updateJewels() {
   //updates the form to reflect changes to jewels and taxes.
 
   taxes = taxes - jewelCost;
-  jewelCost = 1 //ruled * jewelsOwned * 150;
+  jewelCost = 1; //ruled * jewelsOwned * 150;
   jewelsOwned = jewelsOwned + 1;
   updateForm();
 }
@@ -70,18 +69,29 @@ function updateWarCost() {
   ruled = ruled + 1;
   taxes = taxes - warCost;
   warCost = ruled * 50;
+  if (ruled >= numberKingdoms) {
+    //checks to see if the number of kingdoms ruled is greater than the
+    //number of all available kingdoms
+    //declares win if they are
+    //disables the button on the form
+    document.getElementById("ruleworld").innerHTML =
+       "Congratulations!! You rule the world!!";
+    document.getElementById("war").disabled = true;
+    document.getElementById("war").style.backgroundColor = "#948caa";
+  }
+
   updateForm();
 }
 
 function displayCrowns() {
-// hides or shows jewels based on purchases
-// declares win
+  // hides or shows jewels based on purchases
+  // declares win
   let i = 1;
-  
+
   console.log("load crowns " + jewelsOwned);
 
   for (i = 1; i <= maxJewels; i++) {
-    //used to enable/disable crown images based on 
+    //used to enable/disable crown images based on
     //number of jewels owned
     if (i <= jewelsOwned) {
       document.querySelector("#crown" + i).style["display"] = "inline";
@@ -89,40 +99,43 @@ function displayCrowns() {
       document.querySelector("#crown" + i).style["display"] = "none";
     }
   }
-  if (jewelsOwned >= maxJewels){
+  if (jewelsOwned >= maxJewels) {
     //checks to see of all the jewels purchased
     //declares win if they are
     //disables the button onthe form
-      document.getElementById("maxjewel").innerHTML =
-    "Congratulations!! You Win!! You own all the crowns!!";
+    document.getElementById("maxjewel").innerHTML =
+      "Congratulations!! You Win!! You own all the crowns!!";
     document.getElementById("buy-jewels").disabled = true;
-    document.getElementById("button").style.backgroundColor="#948caa";
+    document.getElementById("buy-jewels").style.backgroundColor = "#948caa";
   }
 }
 
 function initializeForm() {
-  //initials the form on form load and 
-  //when user clicks tart over button 
+  //initials the form on form load and
+  //when user clicks tart over button
   startGame = 1;
   taxes = 0;
   ruled = 1;
   jewelsOwned = 1;
-  warCost = 50 
+  warCost = 50;
   jewelCost = 1; //150;
 
   //re-enables buy jewels button if user already won and clears win msg
+  document.getElementById("maxjewel").innerHTML = "";
   document.getElementById("buy-jewels").disabled = false;
-  document.getElementById("maxjewel").innerHTML =   "";
+  document.getElementById("buy-jewels").style.backgroundColor = "#dda0dd";
+  document.getElementById("ruleworld").innerHTML = "";
+  document.getElementById("war").disabled = false;
+  document.getElementById("war").style.backgroundColor = "#dda0dd";
 
-  updateForm();  //writes to the form
-  
+  updateForm(); //writes to the form
 }
 
 function updateForm() {
   //handles writing to the form when taxes are collected
   //wars are waged
   //crowns purchased
-  
+
   document.getElementById("taxescollected").placeholder = "$ " + taxes + "K";
   document.getElementById("kingdomsruled").placeholder = ruled;
   document.getElementById("war-cost").placeholder = "$ " + warCost + "K";
