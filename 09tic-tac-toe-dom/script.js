7d419e997c780531e8b08c705e1df157823be072
Golla <Mounika_Golla@Dell.com>
2018-12-22 15:52:35 -0600

    tic-tac-toedom circleCI Fix02

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -15,37 +15,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
       };
     });
-    });
-    function WinCheck() {
-        const WinningMoves = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        let didPlayerWin = false;
-        WinningMoves.forEach((combo) => {
-            didPlayerWin = (didPlayerWin || (
-                document.querySelector(`[data-cell="${combo[0]}"]`).innerHTML === Player &&
+  });
+  function WinCheck() {
+    const WinningMoves = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    let didPlayerWin = false;
+    WinningMoves.forEach((combo) => {
+      didPlayerWin = (didPlayerWin || (
+        document.querySelector(`[data-cell="${combo[0]}"]`).innerHTML === Player &&
                 document.querySelector(`[data-cell="${combo[1]}"]`).innerHTML === Player &&
                 document.querySelector(`[data-cell="${combo[2]}"]`).innerHTML === Player
-            ));
+      ));
 
-        });
-        if (didPlayerWin) {
-            console.log(`Player ${Player} Won!`);
-            return true;
-        }
-        return false;
+    });
+    if (didPlayerWin) {
+      console.log(`Player ${Player} Won!`);
+      return true;
     }
+    return false;
+  }
 
     function clearBoard() {
-        document.querySelectorAll('[data-cell]').forEach((cell) => {
-            cell.innerHTML = "";
-        });
-    }
+    document.querySelectorAll('[data-cell]').forEach((cell) => {
+      cell.innerHTML = "";
+    });
+  }
 });
