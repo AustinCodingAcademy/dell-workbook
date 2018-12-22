8e46fc67efc012e88e6348ff60da1b6d74037f48
Golla <Mounika_Golla@Dell.com>
2018-12-22 17:58:21 -0600

    Algo

--- 12algorithms/index.js
+++ 12algorithms/index.js
@@ -10,21 +10,88 @@ function getRandomInt(min, max) {
 
 let arr = [];
 
-for (let i = 0; i < 1000; i++) {
-  arr.push(getRandomInt(0, 1000));
+for (let i = 0; i < 10; i++) {
+  arr.push(getRandomInt(0, 10));
 }
 
 function bubbleSort(arr) {
-  // Your code here
+  for(let outer=1;outer<arr.length;outer++ )
+  {
+    for(let inner=0;inner<arr.length;inner++)
+    {
+      if(arr[inner] > arr[inner+1])
+      {
+        let temp = arr[inner]
+        arr[inner] = arr[inner+1];
+        arr[inner+1] = temp;
+      }      
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
-  // Your code here
+  if(arr.length <2)
+    return arr;  
+  const middle = Math.floor(arr.length/2)
+  const left = arr.slice(0,middle)
+  const right = arr.slice(middle)
+
+  return merge(mergeSort(left),mergeSort(right))  
+}
+
+function merge(left,right)
+{
+  let result = []
+  let indexLeft = 0;
+  let indexRight = 0;
+
+  while(indexLeft < left.length && indexRight < right.length){
+    if(left[indexLeft]< right[indexRight]){
+      result.push(left[indexLeft])
+      indexLeft++
+    }     
+    else{
+      result.push(right[indexRight])
+      indexRight++
+    }
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+  }
+
 }
 
 function binarySearch(arr, item) {
-  // Your code here
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start+end)/2);
+
+  while(item !== arr[middle] && start < end)
+  {
+    if(item<arr[middle])
+     end = middle-1
+    else
+     start = middle+1
+     middle = Math.floor((start+end)/2)
+  }
+  return (arr[middle] !== item) ? -1 : middle
+  
+
 }
+
+console.log("BUBBLESORT")
+console.log(arr);
+console.log("sortedarray:")
+console.log(bubbleSort(arr));
+console.log("MERGESORT");
+console.log(mergeSort(arr));
+console.log("BINARY SEARCH");
+let item = 1;
+let result =binarySearch(arr,item) 
+if(result === -1)
+  console.log(`Item ${item} Not found`)
+else
+  console.log(`Item ${item} Found at index ${result}`)
+  
 
 // Tests
 
