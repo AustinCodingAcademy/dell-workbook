e89b2ce49729be34cda12df3a450bfff3dbb6a50
Gurumoorthy <Laxmi_Gurumoorthy@Dell.com>
2018-12-23 09:02:23 -0600

    tic tac toe js

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function(event) {
+/*document.addEventListener("DOMContentLoaded", function(event) {
   let player = "X";
 
   document.querySelectorAll("[data-cell]").forEach(cell => {
@@ -8,9 +8,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
       if (player === "X" && event.target.innerHTML === "") {
         event.target.innerHTML = "X";
         player = "Y";
+        event.target.innerHTML.alert("Winner X")
+                
       } else if (player === "Y" && event.target.innerHTML === "") {
         event.target.innerHTML = "O";
         player = "X";
+        event.target.innerHTML.alert("Winner X")
       }
     });
   });
@@ -18,9 +21,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.querySelector("#clear").addEventListener("click", function() {
     document.querySelectorAll("[data-cell]").forEach(cell => {
       cell.innerHTML = "";
+      event.target.innerHTML.alert("Winner clear")
     });
+    
   });
-});
+});*/
 
 
 
@@ -50,3 +55,74 @@ document.addEventListener("DOMContentLoaded", function(event) {
 //     })
 
 //   })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var board = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+  var wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6] ];
+  var turn = 0;
+  Array.from(document.querySelectorAll('td a'), function (link, i) {
+    link.addEventListener('click', gotClicked.bind(null, link, i));
+  });
+
+  function gotClicked(link, i, e) {
+    e.preventDefault();
+    if (doMove(i, 'You won!')) return;
+    randomAi();
+  }
+
+ 
+  function doMove(i, msg) {
+    var td = document.querySelectorAll('td')[i];
+    var player = turn % 2;
+    td.textContent = 'XO'[player];
+    board[i] = player;
+    turn++;
+    if (isWinFor(player)) {
+        alert(msg);
+        return true; 
+    }
+    if (turn == 9) {
+        alert("It's a draw");
+        return true; 
+    }
+  }
+
+  function randomAi() {
+    var randomPick = Math.floor(Math.random()*(9-turn));
+    for (var i in board) {
+        if (board[i] !== -1) continue; // used cell -- skip it
+        if (randomPick == 0) break; // found it
+        randomPick--;
+    }
+    doMove(i, 'You lost');
+  }
+
+  function isWinFor(player) {
+    for (var win of wins) {
+      var count = 0;
+      for (var index of win) {
+        if (board[index] !== player) break; 
+        count++;
+      }
+      if (count == 3) {
+        Array.from(document.querySelectorAll('td a'), function (link, i) {
+          link.parentNode.innerHTML = ''; 
+        });
+        return true;
+      }
+    }
+  }
+  
+  
+}
+
+);
