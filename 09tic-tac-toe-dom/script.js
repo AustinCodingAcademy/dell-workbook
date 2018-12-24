7a3a9e03d16d2a556d7c6c2af5a215136668d187
Abbas Musabji <amusabji@gmail.com>
2018-12-23 19:07:04 -0600

    09tic-tac-toe-dom complete - no draw/tie functionlity

--- 09tic-tac-toe-dom/script.js
+++ 09tic-tac-toe-dom/script.js
@@ -1,127 +1,159 @@
 "use strict";
 
 // functionality to add later:
-// - figure out which cell was clicked on most recently to optimize win algo
-// - if isClicked = true, mouse:hover shows disabled symbol
 // - font-awesome icons for X/O
+// document.querySelectorAll('[data-cell]').forEach(cell => {
+//   cell.addEventListener('click', function() {
+//     this.innerHTML = '<i class="far fa-circle"></i>';
+//     });
+//   });
 // - show draw when all squares fill up with no winner
-// 
-const initialClickCount = 2;
-let clickCount = initialClickCount;
-let symbol;
-let currentCellSymbol;
-document.querySelectorAll("[data-cell]").forEach(cell => {
-  cell.addEventListener("click", function() {
-    if (cell.innerHTML === "X" || cell.innerHTML === "O") {
-      let currentCellSymbol = this.innerHTML;
-      window.currentCellSymbol = currentCellSymbol;
-      window.symbol = window.currentCellSymbol;
-    }
 
-    determineSymbol();
-    symbol = window.symbol;
-    this.innerHTML = symbol;
-    clickCount++;
-    determineWinner();
+// -----------START TIC-TAC-TOE GAME ---------------------
+
+// ---------- GLOBAL VARIABLES ------------------------
+var Player;
+var winningSymbol;
+
+// first the page loads up the  HTML and CSS
+document.addEventListener("DOMContentLoaded", event => {
+  Player = "X";
+  // then every cell gets a listener attached to it..
+  document.querySelectorAll("[data-cell]").forEach(cell => {
+    cell.addEventListener("click", event => {
+      // if cell block is empty AND there is no winner, do the following:
+      if (!event.target.innerHTML && !winningSymbol) {
+        event.target.innerHTML = Player;
+        determineWinner();
+        //ternary: switch symbols back and forth for both players
+        Player = Player === "X" ? "O" : "X";
+
+        // TO DO: don't allow any more moves after you win..
+        // if (determineWinner() === true) {
+        //   startOver();
+        // }
+        // If you try to click after someone has already won:
+      } else if (winningSymbol) {
+        setTimeout(function() {
+          if (
+            confirm(
+              "This game is over! Player '" +
+                winningSymbol +
+                "' has already won! Want to play again?"
+            )
+          ) {
+            clearBoard();
+          }
+        }, 20); //pause for after winning for 20 milliseconds AKA .02 seconds to let rest of DOM loads first
+      }
+
+      // this is all that happens every time a cell is clicked..
+
+      // first check whether or not a cell has already been clicked so that you can't overwrite existing data..
+
+      //need to assign this variable somewhere, perhaps globally and then ternary
+
+      // this function determines if either symbol wins or if there is a tie.
+    });
   });
+
+  // to do..
+  // function checkCellContents() {
+  //   // if the cell is NOT empty, we have a problem.. alert user.
+  //   if (event.target.innerHTML) {
+  //     prompt("Sorry, this square has already been selected.. Try again.");
+  //     return;
+  //   }
+  // }
+
+  // determine what symbol it is and switch it every valid time.
+  /////////// dont need this in a function...
+  // function switchSymbol() {
+  //   // Teranary
+  //   Player = (Player === 'X') ? 'O' : 'X';
+  // }
+
+  // ------ THIS WORKS!!!!! ---------
+
+  function determineWinner() {
+    // --------- Variables -------------
+    let zero = document.querySelector('[data-cell="0"]').innerHTML;
+    let one = document.querySelector('[data-cell="1"]').innerHTML;
+    let two = document.querySelector('[data-cell="2"]').innerHTML;
+    let three = document.querySelector('[data-cell="3"]').innerHTML;
+    let four = document.querySelector('[data-cell="4"]').innerHTML;
+    let five = document.querySelector('[data-cell="5"]').innerHTML;
+    let six = document.querySelector('[data-cell="6"]').innerHTML;
+    let seven = document.querySelector('[data-cell="7"]').innerHTML;
+    let eight = document.querySelector('[data-cell="8"]').innerHTML;
+
+    if (
+      // check if center cell has a value of 'X' or 'O' or not
+
+      // try: four !== NaN
+      (four === "X" || four === "O") &&
+      // check if opposites from center equal one another:
+
+      // "/"
+      ((six === four && four === two) ||
+        // "\"
+        (zero === four && four === eight) ||
+        // "---"
+        (one === four && four === seven) ||
+        // "|"
+        (three === four && four === five))
+    ) {
+      winningSymbol = four; //you can add a error checker here later
+      document.querySelector("#announce-winner").innerHTML =
+        "Player " + winningSymbol + " Wins!!";
+
+      // this gave me an input field... try "confirm()"
+      // prompt("Hi! You've won!!");
+      return true;
+    } else if (
+      (zero === "X" || zero === "O") &&
+      ((six === three && three === zero) || (zero === one && one === two))
+    ) {
+      winningSymbol = zero; //you can add an error checker here later
+      document.querySelector("#announce-winner").innerHTML =
+        "Player " + winningSymbol + " Wins!!";
+      return true;
+    } else if (
+      (eight === "X" || eight === "O") &&
+      ((five === eight && eight === two) || (six === seven && seven === eight))
+    ) {
+      winningSymbol = eight; //you can add an error checker here later
+      document.querySelector("#announce-winner").innerHTML =
+        "Player " + winningSymbol + " Wins!!";
+      return true;
+    }
+    return false;
+  }
 });
 
-function determineSymbol() {
-  if (window.currentCellSymbol === "X" || window.currentCellSymbol === "O") {
-    window.currentCellSymbol = null;
-    clickCount--;
-  } else if (clickCount % 2 === 0) {
-    let symbol = "X";
-    window.symbol = symbol;
-
-    //this would be cool..
-    //<i class="fas fa-times"></i>;
-  } else if (clickCount % 2 === 1) {
-    let symbol = "O";
-    window.symbol = symbol;
-
-    //this would be cool..
-    //<i class="far fa-circle"></i>;
-  }
-}
-
-// ------ THIS WORKS!!!!! ---------
 function clearBoard() {
   document.querySelectorAll("[data-cell]").forEach(cell => {
     cell.innerHTML = "";
   });
-  clickCount = initialClickCount;
-  window.clickCount = clickCount;
+  // clickCount = initialClickCount;
+  // window.clickCount = clickCount;
   document.querySelector("#announce-winner").innerHTML = "";
+  winningSymbol = null;
+  Player = "X";
 }
 
-function determineWinner() {
+// function startOver() { //figure out how to change the dialogue box..
 
-  // --------- Variables -------------
-  let ax = document.querySelector(".a-x").innerHTML;
-  let bx = document.querySelector(".b-x").innerHTML;
-  let cx = document.querySelector(".c-x").innerHTML;
-  let ay = document.querySelector(".a-y").innerHTML;
-  let by = document.querySelector(".b-y").innerHTML;
-  let cy = document.querySelector(".c-y").innerHTML;
-  let az = document.querySelector(".a-z").innerHTML;
-  let bz = document.querySelector(".b-z").innerHTML;
-  let cz = document.querySelector(".c-z").innerHTML;
+//   if (confirm("Sorry","Sorry, this game is already over! would you like to play again?")) {
+//     clearBoard();
+//   }
+// }
 
-  if (
-    // check if center cell has a value of 'X' or 'O' or not
-    ((by === 'X') || (by === 'O')) && 
-    // check if opposites from center equal one another:
-
-    // "/"
-    (((az === by) && (by === cx)) 
-    ||
-    // "\"
-    ((ax === by) && (by === cz)) 
-    ||
-    // "---"
-    ((bx === by) && (by === bz)) 
-    ||
-    // "|"
-    ((ay === by) && (by === cy)))
-  ) {
-    let winningSymbol = by; //you can add a error checker here later
-    document.querySelector("#announce-winner").innerHTML = "Player " + winningSymbol + " Wins!!";
-    return;
-  } else if (
-
-    ((ax === 'X') || (ax === 'O')) && 
-    (((az === ay) && (ay === ax)) || ((ax === bx) && (bx === cx)))
-  ) {
-    let winningSymbol = ax; //you can add an error checker here later
-    document.querySelector("#announce-winner").innerHTML = "Player " + winningSymbol + " Wins!!";
-    return;
-  }
-  else if (
-    ((cz === 'X') || (cz === 'O')) && 
-    (((cy === cz) && (cz === cx)) || ((az === bz) && (bz === cz)))
-  ) {
-    let winningSymbol = cz; //you can add an error checker here later
-    document.querySelector("#announce-winner").innerHTML = "Player " + winningSymbol + " Wins!!";
-    return;
-  }
-  
-  // ----------------- IT'S A DRAW!!!! --------------------------
-  draw();
-}
-
-function draw() {
-  const initialDrawCount = 0;
-  let drawCount = initialDrawCount;
-  let rando;
-  document.querySelectorAll("[data-cell]").forEach(draw => {
-    let rando = draw.innerHTML;
-    if (rando === "X" || rando === "O") {
-      drawCount++;
-      if (drawCount === 9) {
-        document.querySelector("#announce-winner").innerHTML = "It's a DRAW!";
-      }
-    }
-  });
-  
-}
+// function draw() {
+//   document.querySelectorAll("[data-cell]").forEach(cell => {
+//     if (  ((cell.innerHTML === 'X') || (cell.innerHTML === 'O'))
+//     )
+//       // ----------------- IT'S A DRAW!!!! --------------------------
+//     {document.querySelector("#announce-winner").innerHTML = "It's a DRAW!";}
+//   });
+// }
