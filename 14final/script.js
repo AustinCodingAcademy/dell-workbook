b207c68bd6efa4e1483d1b19aac3a672a0229050
stubclan <aryal.sujan8@gmail.com>
2019-01-06 21:15:01 -0600

    updates on final project

--- 14final/script.js
+++ 14final/script.js
@@ -1,48 +1,60 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // document.querySelector('.cookie').addEventListener('click',()=>{
-  //   document.querySelector('.cookie').className += ' bounce';
-  //   let count = Number(document.querySelector('#counter').innerHTML);
-  //   document.querySelector('#counter').innerHTML = count + 1;
-  // });
-
-  // for (var i = 0; i <=10; i++) {
-  //   createBaloon('red');
-  // }
-
   initialize();
-  var colorArray = ["red", "green", "yellow", "purple", "orange"];
-  var min = 0;
-  var max = 4;
-
-  document.querySelectorAll(".baloon").forEach(baloon => {
-    baloon.addEventListener("click", function(event) {
-      this.parentNode.removeChild(this);
-      //Create Random baloon
-      var randomNumber = min + Math.floor(Math.random() * (max + 1 - min));
-      createBaloon(colorArray[randomNumber]);
-
-      let count = Number(document.querySelector("#counter").innerHTML);
-      document.querySelector("#counter").innerHTML = count + 1;
-    });
-  });
+  addEvent();
 
   function initialize() {
     let count = Number(document.querySelector("#counter").innerHTML);
     if (count === 0) {
       for (var i = 0; i <= 10; i++) {
-        createBaloon("red");
+        createBaloon();
       }
     }
   }
 
-  function createBaloon(color) {
+  function createBaloon() {
+    const colorArray = {
+      1: "red",
+      2: "purple",
+      3: "green",
+      4: "orange",
+      5: "yellow"
+    };
+    let min = 1;
+    let max = 5;
+
+    var randomNumber = min + Math.floor(Math.random() * (max + 1 - min));
+
     var leftposition = Math.floor(Math.random() * 100);
-    var baloon = '<div class="baloon ' + color;
+    var baloon = '<div class="baloon ' + colorArray[randomNumber];
     baloon += '" style="left:' + leftposition + "%;";
     baloon += "z-index:" + leftposition + ";";
     baloon += '"></div>';
-    document.querySelector(".baloon-container").innerHTML += baloon;
+
+    document
+      .querySelector(".baloon-container")
+      .insertAdjacentHTML("beforeend", baloon);
+  }
+
+  function addEvent() {
+    var colorArray = ["none", "red", "purple", "green", "orange", "yellow"];
+
+    document.addEventListener("click", function(e) {
+      let className = e.target.classList[0];
+      if (className === "baloon") {
+        console.log(e.target.classList[1]);
+
+        var colorValue = colorArray.indexOf(e.target.classList[1]);
+
+        let count = Number(document.querySelector("#counter").innerHTML);
+
+        document.querySelector("#counter").innerHTML = count + colorValue;
+        e.target.className += " bounce";
+        e.target.parentNode.removeChild(e.target);
+
+        createBaloon();
+      }
+    });
   }
 });
